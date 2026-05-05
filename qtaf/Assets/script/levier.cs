97d40d9ba31b04c95e2a19643eed3f08d622488a
Amelieve <2496667@cmontmorency.qc.ca>
2026-05-05 17:08:02 -0400

    decoration couloir + lumiere pour reussite levier

--- qtaf/Assets/script/levier.cs
+++ qtaf/Assets/script/levier.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class levier : MonoBehaviour
 {
     public levier_global script_levier_global;
+    public GameObject ampoule1_reussite;
+    public GameObject ampoule2_reussite;
+    public GameObject ampoule3_reussite;
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "levier_control")
@@ -13,19 +16,19 @@ public class levier : MonoBehaviour
             {
                 script_levier_global.levier1 = true;
                 script_levier_global.verification();
-                Debug.Log("levier 1 ");
+                ampoule1_reussite.SetActive(true);
             }
             else if (other.name == "2")
             {
                 script_levier_global.levier2 = true;
                 script_levier_global.verification();
-                Debug.Log("levier 2");
+                ampoule2_reussite.SetActive(true);
             }
             else if (other.name == "3")
             {
                 script_levier_global.levier3 = true;
                 script_levier_global.verification();
-                Debug.Log("levier 3");
+                ampoule3_reussite.SetActive(true);
             }
 
             Debug.Log("tst");
